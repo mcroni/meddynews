a2c9bb7e2da7a116fe09edf36fe18502bc3c86df
kojo mcroni <joeydanieldarko@gmail.com>
2019-09-17 00:56:15 +0000

    creating a news app for news aggregation

--- meddynews/settings.py
+++ meddynews/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+vh$634_01#awss5=t2ft(o!eg#c57@6_*g%yskkfspw^9t+#%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'news.apps.NewsConfig',
 ]
 
 MIDDLEWARE = [
